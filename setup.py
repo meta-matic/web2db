c174aca3882bd20af66d3c3d7a919e77f4cec93d
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2020-02-22 13:30:45 +0530

    modified:   setup.py Added long_description from README.md.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='web2db',
     version='0.1.1',
     description='Fetch webpage full-text, persist link and full text to SQLITE3 db, resumable with tqdm progressbar.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/pushkarparanjpe/web2db',
 	author='Pushkar Paranjpe',
 	author_email='pushkarparanjpe@gmail.com',
