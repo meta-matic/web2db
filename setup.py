6f095bf958a59003257165d3d07894451d99f20d
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2020-09-22 10:59:46 +0530

    - sleep_seconds is user-configurable

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setup(
     name='web2db',
-    version='0.1.1',
+    version='0.1.2',
     description='Fetch webpage full-text, persist link and full text to SQLITE3 db, resumable with tqdm progressbar.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/pushkarparanjpe/web2db',
-	author='Pushkar Paranjpe',
-	author_email='pushkarparanjpe@gmail.com',
-	license='MIT',
-	packages=['web2db'],
-	zip_safe=False
+    author='Pushkar Paranjpe',
+    author_email='pushkarparanjpe@gmail.com',
+    license='MIT',
+    packages=['web2db'],
+    zip_safe=False
 )
